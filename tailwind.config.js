5f0f6f773fea2982e399a55db3798282f8a7db13
cesarzoleko <cesarzoleko@gmail.com>
2025-05-01 19:32:34 +0100

    add responsive side nav and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
       extend:{
+        colors:{
+          "primary":""
+        }
+
       },
+
     },
     variants: {},
     plugins: [],
