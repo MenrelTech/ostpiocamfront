b3f94fdc02445ea9f3f7a97dea88314d1abe136e
joslintchuentep@gmail.com <dev.menrel@gmail.com>
2025-05-05 09:56:50 +0100

    correction count-down

--- src/app/pages/count-down/count-down.component.ts
+++ src/app/pages/count-down/count-down.component.ts
@@ -3,7 +3,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 @Component({
   standalone: true,
   templateUrl: './count-down.component.html',
-  selector:"app-count-down"
+  selector: 'app-count-down'
 })
 export class CountDownComponent implements OnInit, OnDestroy {
   currentDate: Date = new Date();
@@ -11,9 +11,12 @@ export class CountDownComponent implements OnInit, OnDestroy {
   private intervalId: any;
 
   ngOnInit(): void {
-    this.intervalId = setInterval(() => {
-      this.currentDate = new Date();
-    }, 1000);
+    // Vérifiez si l'environnement est un navigateur
+    if (typeof window !== 'undefined') {
+      this.intervalId = setInterval(() => {
+        this.currentDate = new Date();
+      }, 1000);
+    }
   }
 
   ngOnDestroy(): void {
