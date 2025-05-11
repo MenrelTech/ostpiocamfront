09bb7bc42aba750436c5bad1a3643b1a2bbd7146
joslintchuentep@gmail.com <dev.menrel@gmail.com>
2025-05-11 21:27:59 +0100

    build final 1

--- dist/ostpiocamfront/server/angular-app-manifest.mjs
+++ dist/ostpiocamfront/server/angular-app-manifest.mjs
@@ -11,71 +11,84 @@ export default {
   },
   {
     "renderMode": 2,
-    "route": "/spiritualite"
-  },
-  {
-    "renderMode": 2,
-    "route": "/spiritualite/activite"
-  },
-  {
-    "renderMode": 2,
-    "route": "/spiritualite/apropos"
-  },
-  {
-    "renderMode": 2,
-    "route": "/spiritualite/galerie"
-  },
-  {
-    "renderMode": 2,
-    "route": "/sante"
-  },
-  {
-    "renderMode": 2,
-    "route": "/sante/apropos"
-  },
-  {
-    "renderMode": 2,
-    "route": "/sante/galerie"
-  },
-  {
-    "renderMode": 2,
-    "route": "/sante/activite"
-  },
-  {
-    "renderMode": 2,
-    "route": "/environnement"
-  },
-  {
-    "renderMode": 2,
-    "route": "/environnement/apropos"
-  },
-  {
-    "renderMode": 2,
-    "route": "/environnement/activite"
-  },
-  {
-    "renderMode": 2,
-    "route": "/environnement2"
-  },
-  {
-    "renderMode": 2,
     "route": "/actualite"
   },
   {
     "renderMode": 2,
-    "route": "/communication"
-  },
-  {
-    "renderMode": 2,
-    "route": "/communication/apropos"
-  },
-  {
-    "renderMode": 2,
-    "route": "/communication/activite"
-  },
-  {
-    "renderMode": 2,
+    "redirectTo": "/champs d'apostolat/spiritualite",
     "route": "/champs d'apostolat"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/champs d'apostolat/spiritualite/apropos",
+    "route": "/champs d'apostolat/spiritualite"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/spiritualite/apropos"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/spiritualite/activite"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/spiritualite/galerie"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/spiritualite/connaissance de l'eglise"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/champs d'apostolat/sante/apropos",
+    "route": "/champs d'apostolat/sante"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/sante/apropos"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/sante/activite"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/sante/galerie"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/champs d'apostolat/communication/apropos",
+    "route": "/champs d'apostolat/communication"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/communication/apropos"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/communication/activite"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/communication/galerie"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/champs d'apostolat/environnement/apropos",
+    "route": "/champs d'apostolat/environnement"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/environnement/apropos"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/environnement/activite"
+  },
+  {
+    "renderMode": 2,
+    "route": "/champs d'apostolat/environnement/galerie"
   },
   {
     "renderMode": 2,
@@ -124,37 +137,34 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 7498, hash: 'a017e7d3c5757508a69338ab6d255a4595d36f3d5c38354b2b6599f628d5ea4d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1011, hash: '6ed012d0dcc818f4339b40a607c03d68beafd2269bf11010ac31fa50c06dbcc9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'spiritualite/index.html': {size: 86260, hash: 'a3c5fcff1958be842cc40cf68f74b246a9f1e23a2b23543288815843961f660a', text: () => import('./assets-chunks/spiritualite_index_html.mjs').then(m => m.default)},
-    'spiritualite/galerie/index.html': {size: 55984, hash: '3b9e2b7ad886c89c8455d2e85d4410391bb9e7b76e1612ec8cebffc45890d670', text: () => import('./assets-chunks/spiritualite_galerie_index_html.mjs').then(m => m.default)},
-    'sante/index.html': {size: 79381, hash: 'bca3309c2500f98c695994d9c35100d6665536d25115b6ccd36e1412d7c1ef10', text: () => import('./assets-chunks/sante_index_html.mjs').then(m => m.default)},
-    'sante/apropos/index.html': {size: 55500, hash: '4f0946671e70d1fc32ac8b0672c51a150b9194e23fd9a56deaf824397cc3121f', text: () => import('./assets-chunks/sante_apropos_index_html.mjs').then(m => m.default)},
-    'spiritualite/activite/index.html': {size: 80454, hash: 'a05904c45a257e886a9c85f3b97442fcb8d950f1b8c137ba9335fe4129bae1b2', text: () => import('./assets-chunks/spiritualite_activite_index_html.mjs').then(m => m.default)},
-    'spiritualite/apropos/index.html': {size: 56776, hash: '9365a9f50ddfadabbef6d1400c0798e9bb6b8c7855a943e6a42efd5cce99d7f2', text: () => import('./assets-chunks/spiritualite_apropos_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 142162, hash: '9ed3977dbffee14cf15b03e5495faf82309f357f3cfedf9c7fb76f423ad0fb3d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'sante/galerie/index.html': {size: 54983, hash: '77d3cf94478f751e8d5403414df4868e25ce1aafad58d01de56718f07c4d1e9d', text: () => import('./assets-chunks/sante_galerie_index_html.mjs').then(m => m.default)},
-    'sante/activite/index.html': {size: 69942, hash: 'c712af50e1c786a123d9385bfe13d1f27ad8d56b6e4b391042d5765a37785962', text: () => import('./assets-chunks/sante_activite_index_html.mjs').then(m => m.default)},
-    'environnement/index.html': {size: 83697, hash: '2f03740d60538bc8bb0354cb263f9138cf9a10a1f6abd00275d029df288915e0', text: () => import('./assets-chunks/environnement_index_html.mjs').then(m => m.default)},
-    'environnement/activite/index.html': {size: 69897, hash: '2c296842f12fbcdcf72cc663ebcdf4f831ca8882eae2cf755d460644112f5b09', text: () => import('./assets-chunks/environnement_activite_index_html.mjs').then(m => m.default)},
-    'environnement/apropos/index.html': {size: 55568, hash: '70d61e9e15db3367004f9ba80f9ea3ec95856ac824853528e56e6856bfa28131', text: () => import('./assets-chunks/environnement_apropos_index_html.mjs').then(m => m.default)},
-    'environnement2/index.html': {size: 81773, hash: 'f292609fecd9d1f25110e511f93ddb6dbb7b12bf599fcbf07bc8dfa9f614085d', text: () => import('./assets-chunks/environnement2_index_html.mjs').then(m => m.default)},
-    'communication/index.html': {size: 69487, hash: '2b20afbbe7eb5a824a0eea4dac5adaea442b7f761c510b0e8c5f10ec9204f143', text: () => import('./assets-chunks/communication_index_html.mjs').then(m => m.default)},
-    'communication/activite/index.html': {size: 69873, hash: 'a76d54a0b8f5b2b1ac023e5c0774ceb729a8b47bd91578d84093a09e31235056', text: () => import('./assets-chunks/communication_activite_index_html.mjs').then(m => m.default)},
-    'communication/apropos/index.html': {size: 55493, hash: '9f9e1bf0af3df64a3ca1b1f22eb726b1bda7aa97a6570b535381a477812bea80', text: () => import('./assets-chunks/communication_apropos_index_html.mjs').then(m => m.default)},
-    'actualite/index.html': {size: 65492, hash: '8637d8439eecd8c50431a72c9dff973cc8bd7eab9b0709f27572f4b23296c5ad', text: () => import('./assets-chunks/actualite_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 57728, hash: '634f4f446e941b047924410f81cb735f25499f796f75f303a1fdebfec6435265', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'champs d'apostolat/index.html': {size: 45527, hash: '8cff52f9b224eafbde9a7b8b1f45f8a707a6bea61e10013317d5036eccc4dc2f', text: () => import('./assets-chunks/champs d'apostolat_index_html.mjs').then(m => m.default)},
-    'inscription/index.html': {size: 48699, hash: '1324819b4e99fa15f1b4fc2450ced7ffd37829d107184bca037c3c6388cc6990', text: () => import('./assets-chunks/inscription_index_html.mjs').then(m => m.default)},
-    'connexion/index.html': {size: 47496, hash: '3968c9c4c94497e62caa5b1885a9e4cfc6f834299a5532f9aef48f3200b5fce7', text: () => import('./assets-chunks/connexion_index_html.mjs').then(m => m.default)},
-    'news-letter/index.html': {size: 47552, hash: 'c1eb43f17e43796d1f8753f1efee0861bf7d2b70ec6a3dad9385d51eeba8f2e9', text: () => import('./assets-chunks/news-letter_index_html.mjs').then(m => m.default)},
-    'devenir-benevole/index.html': {size: 53383, hash: '8e043c4ddf2dec01707d169312ed8166b2b2ecec0d2435abadc1a0376bf3e4af', text: () => import('./assets-chunks/devenir-benevole_index_html.mjs').then(m => m.default)},
-    'count-down/index.html': {size: 46585, hash: 'c2c32927889d2564dd961064dbc621a3c7740871cb23b7e92fed35f36596b213', text: () => import('./assets-chunks/count-down_index_html.mjs').then(m => m.default)},
-    'devenir-partenaire/index.html': {size: 52203, hash: '0a6708528a98851a48d923b378e2eae063f58218677d3640ed730ebe1bc0b500', text: () => import('./assets-chunks/devenir-partenaire_index_html.mjs').then(m => m.default)},
-    'changer-de-mot-de-passe/index.html': {size: 46787, hash: 'd921a51e9f1a462333bca6e12b370106af77e33bc7a2fbc13901543db9d49f1a', text: () => import('./assets-chunks/changer-de-mot-de-passe_index_html.mjs').then(m => m.default)},
-    'presentation/index.html': {size: 49514, hash: 'c8a5f8ec71d21e00375bbd6e04b98813e96e0434b5a23442f1f319c6ca93772b', text: () => import('./assets-chunks/presentation_index_html.mjs').then(m => m.default)},
-    'prolegomene/index.html': {size: 48953, hash: 'c62cab198d4a044c5270cb81fcea2631e785a7e1daa78b6c16db449af21356d4', text: () => import('./assets-chunks/prolegomene_index_html.mjs').then(m => m.default)},
-    'info-user/index.html': {size: 46435, hash: '9a5222fb6dfb2c85d06f92220e6b54605aea608e84c5b6c45a58cdba1e4d21ff', text: () => import('./assets-chunks/info-user_index_html.mjs').then(m => m.default)},
-    'styles-KXWASEYL.css': {size: 38176, hash: '+Jfp8cIEbuc', text: () => import('./assets-chunks/styles-KXWASEYL_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7498, hash: '1938cb821eb76df10c92b6a2c6988e51376fd5e0a3c67ad127ba55a543360138', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1011, hash: '14609c3ec59ddf56e776cefec7a2164511fadf643fcb496c86f77d5fcd2699e3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'champs d'apostolat/spiritualite/apropos/index.html': {size: 88298, hash: 'f2e3251dc5cacda17b52c335c8b41b4e4eeb832bc1e2ca60dc6356488bc8cef2', text: () => import('./assets-chunks/champs d'apostolat_spiritualite_apropos_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/spiritualite/galerie/index.html': {size: 54375, hash: '2996f4bab5906de7b9b2e6a788fae76c12e203d50557bb27b175c3bc12436e97', text: () => import('./assets-chunks/champs d'apostolat_spiritualite_galerie_index_html.mjs').then(m => m.default)},
+    'actualite/index.html': {size: 45898, hash: '70622fdfc0775dc9816b04336118a1fa283e7269502b1641a7646bb91cb44bf5', text: () => import('./assets-chunks/actualite_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 135368, hash: '3c4cc6c916b55bff7dc5c8b2c13bd03bded37cda6d2c9381b6865fa242ccdb1b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/spiritualite/connaissance de l'eglise/index.html': {size: 81217, hash: 'd30672978b8b728fcc439745fc276694b6aaccf782e6b3a1fa8347ec218bede5', text: () => import('./assets-chunks/champs d'apostolat_spiritualite_connaissance de l'eglise_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/spiritualite/activite/index.html': {size: 71562, hash: 'c4e204c0a4912ecfee3f622b1ca8a44cc53605be44e47a1ad75aba9a6aff70f7', text: () => import('./assets-chunks/champs d'apostolat_spiritualite_activite_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/sante/activite/index.html': {size: 70711, hash: 'a23e3a06103751176ebe6fe2018a22dd766d014029d64ac671bfb57e39a0b8cd', text: () => import('./assets-chunks/champs d'apostolat_sante_activite_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/sante/apropos/index.html': {size: 80110, hash: '63f029c13512cc745bb5bf4bdcabb14c3b78b441874fd4cccb258dd83018e9a6', text: () => import('./assets-chunks/champs d'apostolat_sante_apropos_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/communication/activite/index.html': {size: 70747, hash: 'ba4bd5927dea05416abbb618843fb68872ec9153adc091053d18668d6706932f', text: () => import('./assets-chunks/champs d'apostolat_communication_activite_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/communication/apropos/index.html': {size: 67394, hash: '975e93b19f6977869678472a6319009d8a716484460a1986068923c5565a9fdd', text: () => import('./assets-chunks/champs d'apostolat_communication_apropos_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/environnement/apropos/index.html': {size: 56315, hash: '05a8c5a3113bf3778974903909307403f6946cbb93e6991044e25fd9b321d2e4', text: () => import('./assets-chunks/champs d'apostolat_environnement_apropos_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/environnement/activite/index.html': {size: 70822, hash: '26e77ed17a58b8fa23323f8eb4ea3bb785165fe3d3819cd393e4f744fd1f6cad', text: () => import('./assets-chunks/champs d'apostolat_environnement_activite_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/sante/galerie/index.html': {size: 55974, hash: '66c3c5d5b73360da3065f9483056fd90b171e29832a2b1694fc2e51d42279abd', text: () => import('./assets-chunks/champs d'apostolat_sante_galerie_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/communication/galerie/index.html': {size: 54347, hash: '423d2efd2d1c4cce58683944c7930aac2c2fefd57e93b70eeec58710cc8c0986', text: () => import('./assets-chunks/champs d'apostolat_communication_galerie_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 57931, hash: '9cd08c4ed95001739870e476aeab895387f74c5b4b38a82000a1e08a310fa496', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'connexion/index.html': {size: 47701, hash: '7da86a0e78745c0ff96483f061071854ffa398a191a257fd98a373d6d8c89665', text: () => import('./assets-chunks/connexion_index_html.mjs').then(m => m.default)},
+    'inscription/index.html': {size: 48906, hash: 'aaa1e4396c82ecfb03a841dafb4dd933867a5d168451994dd93fc62699d210e7', text: () => import('./assets-chunks/inscription_index_html.mjs').then(m => m.default)},
+    'devenir-benevole/index.html': {size: 53586, hash: '807ff8adf549f8eae1e9407f17aaf4e607477a75559a156212ef9ada09bd94d9', text: () => import('./assets-chunks/devenir-benevole_index_html.mjs').then(m => m.default)},
+    'champs d'apostolat/environnement/galerie/index.html': {size: 54362, hash: 'f9bf8d04962eb9dbb0941ffbd19f3c6203ed1e7f144787c8ae81f3ff5ef82018', text: () => import('./assets-chunks/champs d'apostolat_environnement_galerie_index_html.mjs').then(m => m.default)},
+    'news-letter/index.html': {size: 47862, hash: '3d3fa6fdf6a7b4227fe884d6678560bc5a15564fa3d322d48e7bab43fc04cfa4', text: () => import('./assets-chunks/news-letter_index_html.mjs').then(m => m.default)},
+    'count-down/index.html': {size: 46792, hash: '694406bbb2c69de2b99edccc91d19d7b0413b5955c811f3647a1d35f6cee0446', text: () => import('./assets-chunks/count-down_index_html.mjs').then(m => m.default)},
+    'devenir-partenaire/index.html': {size: 52410, hash: '857e6260caed3b606be0d414eceafe18e82b815deeb1113a93cb9b5a7b518950', text: () => import('./assets-chunks/devenir-partenaire_index_html.mjs').then(m => m.default)},
+    'presentation/index.html': {size: 49721, hash: '5735594d6d0c817c022917f3557ea8911644ba756a7a0bd16f9e008ed5125bf8', text: () => import('./assets-chunks/presentation_index_html.mjs').then(m => m.default)},
+    'info-user/index.html': {size: 46629, hash: '3315d2534685082fa2a8575b58895a9d486f679057f79afb9cfc30d45b5f7e56', text: () => import('./assets-chunks/info-user_index_html.mjs').then(m => m.default)},
+    'changer-de-mot-de-passe/index.html': {size: 46994, hash: 'e5bb69397adc1de7cba72bc7bdeccbaa4b89bd06e7fa5c740442a15183aa0e6f', text: () => import('./assets-chunks/changer-de-mot-de-passe_index_html.mjs').then(m => m.default)},
+    'prolegomene/index.html': {size: 49156, hash: '4df142be9162a69143ec467b0324f4774320bcdbbef559aa46100b595f029431', text: () => import('./assets-chunks/prolegomene_index_html.mjs').then(m => m.default)},
+    'styles-IMLKA6ZJ.css': {size: 38340, hash: 'TtuZjiQ6j6c', text: () => import('./assets-chunks/styles-IMLKA6ZJ_css.mjs').then(m => m.default)}
   },
 };
